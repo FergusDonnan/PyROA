8c8571373e684d095b73dfe768e0801283343ed6
Juan V. Hernandez Santisteban <Alymantara@users.noreply.github.com>
2023-03-27 11:51:43 +0100

    Update __init__.py to import ROA

--- PyROA/__init__.py
+++ PyROA/__init__.py
@@ -1,7 +1,7 @@
 __modules__ = ['PyROA']
 #import ROA
 
-from .PyROA import Fit, InterCalibrate, GravLensFit, Plot
+from .PyROA import Fit, InterCalibrate, GravLensFit, Plot, RunningOptimalAverage
 from .Utils import Chains, Lightcurves, FluxFlux, LagSpectrum, CornerPlot, Convergence
 
 __version__ = "3.1.0"
